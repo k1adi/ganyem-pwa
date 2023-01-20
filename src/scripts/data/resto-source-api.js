a42cb7b1622bd299ce9938c795ee27a60f5504b1
Rizki Adi <rizkiadinugroho3@gmail.com>
2023-01-21 01:45:01 +0700

    added data source

---
+++ src/scripts/data/resto-source-api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import API_ENDPOINT from '../global/api-endpoint';
+
+class RestoSource {
+  static async getAllResto() {
+    try {
+      const response = await axios.get(API_ENDPOINT.GET_ALL_RESTO);
+      return Promise.resolve(response.data);
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+}
+
+export default RestoSource;
