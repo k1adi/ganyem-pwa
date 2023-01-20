e6e1c60f6288152dfd10a9bf83e74daa16cd6dff
Rizki Adi <rizkiadinugroho3@gmail.com>
2023-01-21 01:45:33 +0700

    create config and api endpoint

---
+++ src/scripts/global/api-endpoint.js
@@ -0,0 +1,10 @@
+import CONFIG from './config';
+
+const API_ENDPOINT = {
+  GET_ALL_RESTO: `${CONFIG.API_BASE_URL}/list`,
+  GET_IMG_RESTO: (size) => `${CONFIG.API_BASE_URL}/images/${size}`,
+  GET_DETAIL_RESTO: (id) => `${CONFIG.API_BASE_URL}/detail${id}`,
+  POST_REVIEW: `${CONFIG.API_BASE_URL}/review`,
+};
+
+export default API_ENDPOINT;
---
+++ src/scripts/global/config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  LOCAL_STORAGE_THEME: 'ganyem-theme',
+  API_BASE_URL: 'https://restaurant-api.dicoding.dev',
+};
+
+export default CONFIG;
