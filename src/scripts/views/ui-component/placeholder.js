34a9b7e89ad06ddcfc1ac1dab637e25221dcf9c1
Rizki Adi <rizkiadinugroho3@gmail.com>
2023-01-23 11:48:32 +0700

    create placeholder component

--- src/scripts/views/ui-component/placeholder.js
+++ src/scripts/views/ui-component/placeholder.js
@@ -17,7 +17,55 @@ const createRestoPlaceholder = () => {
 };
 
 const createRestoDetailPlaceholder = () => {
+  let placeholderDesc = '';
+  let placeholderSection = '';
 
+  // eslint-disable-next-line no-plusplus
+  for (let maxDesc = 0; maxDesc < 8; maxDesc++) {
+    placeholderDesc += `
+      <span>
+        <div class="placeholder">
+          <div class="animation"></div>
+        </div>
+      </span>
+    `;
+  }
+
+  // eslint-disable-next-line no-plusplus
+  for (let maxSection = 0; maxSection < 3; maxSection++) {
+    placeholderSection += `
+      <div class="detail-card__placeholder--section">
+        <div class="placeholder">
+          <div class="animation"></div>
+        </div>
+      </div>
+    `;
+  }
+
+  return `
+  <div class="detail-wrapper__info">
+    <div class="detail-card">
+      <div class="detail-card__image">
+        <div class="placeholder">
+          <div class="animation"></div>
+        </div>
+      </div>
+
+      <div class="detail-card__placeholder--name">
+        <div class="placeholder">
+          <div class="animation"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="detail-wrapper__desc">
+    <div class="detail-card__placeholder--desc">
+      ${placeholderDesc}
+    </div>
+    
+    ${placeholderSection}
+  </div>
+  `;
 };
 
 const removeCurrentPlaceholder = (parentElement) => {
