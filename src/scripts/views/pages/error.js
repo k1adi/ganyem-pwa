5fe2cc3e6e929a84a774da2771e6f1fae0efff44
Rizki Adi <rizkiadinugroho3@gmail.com>
2023-01-24 12:05:40 +0700

    added not found page

--- src/scripts/views/pages/error.js
+++ src/scripts/views/pages/error.js
@@ -1,10 +1,21 @@
+import notFoundImag from '../../../images/not-found.svg';
+
 const ErrorPage = {
   async render() {
-    console.log('render error');
+    return `
+      <div class="error-wrapper">
+        <div class="error-content">
+          <img src="${notFoundImag}" alt="">
+          <h1>Oops...</h1>
+          <p class="text--muted">Page is not found</p>
+          <a href="/#">Back to Home</a>
+        </div>
+      </div>
+    `;
   },
 
   async afterRender() {
-    console.log('after render error');
+    console.error('Oops, page is not found');
   },
 };
 
